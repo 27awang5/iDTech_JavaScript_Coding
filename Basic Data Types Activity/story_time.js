1d6b6f5561e9bbd9961e8d64006d56b128b2e219
27awang5 <141187918+27awang5@users.noreply.github.com>
2024-07-19 10:39:46 -0400

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,32 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let mainCharacter = "Bob";
+let friend = "Ian";
+let month = "July";
+let mood = "uncertain";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year = 2024;
+let monthsSinceMet = 10;
+let monthsSinceGone = 1;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let emotions = ["Confused", "sad", "happy", "numb", "uncertain", "tempermental"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let gettingBy = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("The year is " + year);
+console.log(mainCharacter + " is feeling " + mood);
+console.log(friend + ", his friend, has grown distant and left " + mainCharacter + "'s life");
+console.log(mainCharacter + " does not know why");
+console.log("It is " + month + " now, " + monthsSinceMet + " months since they met and " + monthsSinceGone + " since they separated");
+console.log(mainCharacter + " replays his experiences with " + friend + " in his head");
+console.log("He feels a ton of emotions about it, many of which contradict each other");
+console.log(emotions[0] + ", " + emotions[1] + ", " + emotions[2] + ", " + emotions[5]);
+console.log("Whether he's getting by, though: " + gettingBy);
